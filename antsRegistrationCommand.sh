d828d2d3d9609b1f014d399b03c1f688cb9c0119
BBAvants <stnava@gmail.com>
2014-10-28 08:57:28 -0400

    ENH: k nhood

--- antsRegistrationCommand.sh
+++ antsRegistrationCommand.sh
@@ -4,20 +4,21 @@ movingPoints='data/chicken-3.nii.gz'
 outputDirectory='./'
 outputPrefix=${outputDirectory}/pseChicken4xChicken3
 s=0.1 # sampling rate
+k=10 # k - neighborhood
 antsRegistration -d 2 \
                  -o ${outputPrefix} \
                  -r [${fixedPoints},${movingPoints},1] \
-                 -m PSE[${fixedPoints},${movingPoints},1,${s},1,20,50] \
+                 -m PSE[${fixedPoints},${movingPoints},1,${s},1,20,$k] \
                  -t Affine[0.1] \
                  -c [50,0,10] \
                  -s 0 \
                  -f 1 \
-                 -m PSE[${fixedPoints},${movingPoints},1,${s},1,10,50] \
+                 -m PSE[${fixedPoints},${movingPoints},1,${s},1,10,$k] \
                  -t Affine[0.1] \
                  -c [40,0,10] \
                  -s 0 \
                  -f 1 \
-                 -m PSE[${fixedPoints},${movingPoints},1,${s},1,5,50] \
+                 -m PSE[${fixedPoints},${movingPoints},1,${s},1,5,$k] \
                  -t Affine[0.1] \
                  -c [20,0,10] \
                  -s 0 \
