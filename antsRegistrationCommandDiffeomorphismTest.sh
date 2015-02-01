ae7209f13fc9ebff152ef3659fb887ff24f73743
stnava <stnava@gmail.com>
2015-02-01 10:40:31 -0500

    DOC: InvId

--- antsRegistrationCommandDiffeomorphismTest.sh
+++ antsRegistrationCommandDiffeomorphismTest.sh
@@ -21,7 +21,8 @@ antsRegistration -d 2 \
                  -c [200x200x200x200x50,0,10] \
                  -s 0x0x0x0x0 \
                  -f 8x8x4x2x1
- antsApplyTransforms -d 2 \
+
+antsApplyTransforms -d 2 \
                      -o ${outputPrefix}Warped.nii.gz \
                      -i ${movingPoints} \
                      -r ${fixedPoints} \
@@ -35,9 +36,10 @@ antsApplyTransforms -d 2 \
                     -n NearestNeighbor \
                     -t ${outputPrefix}0InverseWarp.nii.gz
 
+ImageMath 2 ${outputPrefix}invid.nii.gz InvId ${outputPrefix}0InverseWarp.nii.gz ${outputPrefix}0Warp.nii.gz
 
 CreateWarpedGridImage 2 ${outputPrefix}0Warp.nii.gz ${outputPrefix}Grid.nii.gz
 
 CreateWarpedGridImage 2 ${outputPrefix}0InverseWarp.nii.gz ${outputPrefix}InvGrid.nii.gz
 # now visualize with snap
-#  snap -g diffeomorphismTestWarpedGrid.nii.gz -s diffeomorphismTestWarped.nii.gz
+#  snap -g diffeomorphismTestGrid.nii.gz -s diffeomorphismTestWarped.nii.gz
