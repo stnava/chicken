3612a58fa2bc9940b285f31599ae542b4971514b
stnava <stnava@gmail.com>
2015-03-03 05:30:14 -0500

    ENH: manual lm ex

--- runthis.sh
+++ runthis.sh
@@ -29,3 +29,7 @@ cat chicken-3w.csv test.csv
 # these should produce identical output, up to file format
 antsApplyTransformsToPoints -d 2 -i ./data/chicken-3-ref.csv -o testq2.csv -t [ data/chicken3to4-ref.mat , 1]
 antsApplyTransformsToPoints -d 2 -i ./data/chicken-3-ref.mha -o testq2.mha -t [ data/chicken3to4-ref.mat , 1]
+
+# a manual example
+ANTSUseLandmarkImagesToGetAffineTransform data/chicken-3-seg.nii.gz data/chicken-4-seg.nii.gz affine aff.txt
+antsApplyTransforms -d 2 -i data/chicken-4.nii.gz -o /tmp/chk.nii.gz -t aff.txt -r data/chicken-3.nii.gz 
